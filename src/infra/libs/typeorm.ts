22603857c0bd5344ffcd24ba04cb91ac05f5e6e6
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2024-06-28 07:49:58 -0300

    refactor: remove ROOT_DIR and POSTGRESQL_SCHEMA env, instead got schema from url

--- src/infra/libs/typeorm.ts
+++ src/infra/libs/typeorm.ts
@@ -4,10 +4,13 @@ import { env } from '../env';
 import { TypeormAuditModel } from '../databases/typeorm/models/typeorm-audit-model';
 import { CreateAudits1719167162794 } from '../databases/typeorm/migrations/1719167162794-create-audits';
 
+const schema =
+  new URL(env.POSTGRESQL_URL).searchParams.get('schema') ?? 'public';
+
 export const appDataSource = new DataSource({
   type: 'postgres',
   url: env.POSTGRESQL_URL,
-  schema: env.POSTGRESQL_SCHEMA,
+  schema,
   entities: [TypeormAuditModel],
   migrations: [CreateAudits1719167162794],
   migrationsRun: true,
